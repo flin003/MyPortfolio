1cf3103ac93c0494e07cb127d284b3a6bb79df28
flin003 <linfanlf2024@163.com>
2026-04-18 03:34:06 +0100

    First

---
+++ script.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    loop: true, // 循环滑动
+    effect: "slide", // 切换效果
+});
